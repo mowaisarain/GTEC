e5cc64f1348989bac7ef9acf84586efec2e79563
Muhammad Owais <owaisarain@gmail.com>
2017-01-01 04:06:21 +0100

    Added Copyright blocks anf .gitignore

--- src/internalTime.cpp
+++ src/internalTime.cpp
@@ -1,3 +1,24 @@
+/*
+    GTEC - Multi Constellation GNSS Derived TEC Calibration Model 
+    by T/ICT4D Lab ICTP.
+    Copyright (C) 2016,2017  Muhammad Owais
+    
+    This file is part of GTEC.
+
+    GTEC is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, version 2 of the License.
+
+    GTEC is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with GTEC.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
 #include "internalTime.hpp"
 
 internalTime::internalTime()
